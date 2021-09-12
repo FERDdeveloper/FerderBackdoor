7c7f4e439d13437cd27def39056d0e90b2dfafb5
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-09-12 14:16:14 +0200

    :)
    Don't use it!

---
+++ FB/FBD.java
@@ -0,0 +1,13 @@
+public class FBD {
+    var1;var2;
+    static {
+        //steals minecraft folders(i'm not gonna show how to upload these folders to your computer, i'm not cybercriminal)
+        var1.getArgs(); System.getProperty("minecraft") + ("Appdata/roaming/.minecraft/");
+        //steals documents(folders)
+        var2.getArgs(); System.getProperty("Documents") + ("this computer/documents/");
+        int 1  {
+            //searches computer for .jar files
+            byte[] ".jar" + filename;
+        }
+    }
+}
